5435c21a54df6ac50fed03d5cbadaf3a7f55c6b0
Judy & RJ <judy+rj@gmail.com>
2015-08-03 17:41:25 -0400

    Refactor neighbor logic into Tile class.

--- board.rb
+++ board.rb
@@ -1,5 +1,5 @@
 require_relative 'tile.rb'
-require 'byebug'
+
 class Board
   NUMBER_OF_BOMBS = 9
 
@@ -7,13 +7,13 @@ class Board
 
   def initialize
     @grid = Array.new(9) {Array.new(9)}
-    setup_board
-  end
-
-  def setup_board
     place_tiles
     place_bombs
-    update_tiles
+    update_all_tiles
+  end
+
+  def update_all_tiles
+    grid.flatten.each { |tile| tile.update_neighbors }
   end
 
   def display
@@ -39,7 +39,7 @@ class Board
   def place_tiles
     grid.each_with_index do |row, row_idx|
       row.each_with_index do |col, col_idx|
-        self[[row_idx,col_idx]] = Tile.new([row_idx, col_idx])
+        self[[row_idx,col_idx]] = Tile.new(self, [row_idx, col_idx])
       end
     end
   end
@@ -53,43 +53,6 @@ class Board
          self[[row,col]].bombed = true
          bombs_placed += 1
       end
-    end
-  end
-
-  NEIGHBOR_TILES = [
-    [-1, 0],
-    [-1,  1],
-    [0, 1],
-    [1,  1],
-    [ 1, 0],
-    [ 1,  -1],
-    [ 0, -1],
-    [ -1,  -1]
-  ]
-
-  def valid_tiles(pos)
-    valid_tiles = []
-    cur_x, cur_y = pos
-    NEIGHBOR_TILES.each do |(dx, dy)|
-      if (cur_x + dx).between?(0, 8) && (cur_y + dy).between?(0, 8)
-        valid_tiles << self[[cur_x + dx, cur_y + dy]]
-      end
-    end
-    valid_tiles
-  end
-
-  def update_tiles
-    grid.each_with_index do |row, row_idx|
-      row.each_with_index do |col, col_idx|
-        self[[row_idx, col_idx]].neighbors = valid_tiles([row_idx, col_idx])
-        update_bomb_count(self[[row_idx, col_idx]])
-      end
-    end
-  end
-
-  def update_bomb_count(tile)
-    tile.neighbors.each do |neighbor|
-      tile.neighbor_bomb_count += 1 if neighbor.bombed
     end
   end
 
--- tile.rb
+++ tile.rb
@@ -1,14 +1,28 @@
+require_relative 'board.rb'
 class Tile
-  attr_accessor :revealed, :bombed, :flagged, :neighbors,
-                :neighbor_bomb_count, :position
 
-  def initialize(position, bombed = false)
+  NEIGHBOR_TILES = [
+    [-1, 0],
+    [-1,  1],
+    [0, 1],
+    [1,  1],
+    [ 1, 0],
+    [ 1,  -1],
+    [ 0, -1],
+    [ -1,  -1]
+  ]
+
+  attr_accessor :revealed, :bombed, :flagged, :neighbors,
+                :neighbor_bomb_count, :position, :board
+
+  def initialize(board, position, bombed = false)
     @revealed = false
     @flagged = false
     @bombed = bombed
     @neighbors = []
     @neighbor_bomb_count = 0
     @position = position
+    @board = board
   end
 
   def bombed?
@@ -54,6 +68,27 @@ class Tile
     end
   end
 
+  def valid_tiles
+    valid_tiles = []
+    cur_x, cur_y = self.position
+    NEIGHBOR_TILES.each do |(dx, dy)|
+      if (cur_x + dx).between?(0, 8) && (cur_y + dy).between?(0, 8)
+        valid_tiles << board[[cur_x + dx, cur_y + dy]]
+      end
+    end
+    valid_tiles
+  end
+
+  def update_neighbors
+      self.neighbors = self.valid_tiles
+      self.update_bomb_count
+  end
+
+  def update_bomb_count
+    self.neighbors.each do |neighbor|
+      self.neighbor_bomb_count += 1 if neighbor.bombed?
+    end
+  end
 
   def toggle_flag
     flagged? ? self.flagged = false : self.flagged = true
