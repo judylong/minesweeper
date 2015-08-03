9f5773862a74ae92a1f123a323ba050d4b1917e6
Judy & RJ <judy+rj@gmail.com>
2015-08-03 12:16:22 -0400

    Add Board#place_tiles.

--- board.rb
+++ board.rb
@@ -1,8 +1,28 @@
+require_relative 'tile.rb'
+
 class Board
   attr_accessor :grid
 
   def initialize
     @grid = Array.new(9) {Array.new(9)}
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, tile)
+    x, y = pos
+    self.grid[x][y] = tile
+  end
+
+  def place_tiles
+    grid.each_with_index do |row, row_idx|
+      row.each_with_index do |col, col_idx|
+        self[[row_idx,col_idx]] = Tile.new
+      end
+    end
   end
 
 end
