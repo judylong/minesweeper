1c4f475bde509fd140fb2329e11c2ddf88352fc8
Judy & RJ <judy+rj@gmail.com>
2015-08-03 13:22:27 -0400

    Add position variable and accessor to Tile class.

--- tile.rb
+++ tile.rb
@@ -1,12 +1,18 @@
 class Tile
-  attr_accessor :revealed, :bombed, :flagged, :neighbors, :neighbor_bomb_count
+  attr_accessor :revealed, :bombed, :flagged, :neighbors,
+                :neighbor_bomb_count, :position
 
-  def initialize(bombed = false)
+  def initialize(bombed = false, position)
     @revealed = false
     @flagged = false
     @bombed = bombed
     @neighbors = []
     @neighbor_bomb_count = 0
+    @position = position
+  end
+
+  def find_neighbors
+
   end
 
 end
