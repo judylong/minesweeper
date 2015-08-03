7c89d3c00be8f7c463502800275e443cb1966ac5
Judy & RJ <judy+rj@gmail.com>
2015-08-03 12:24:01 -0400

    Add Board#place_bombs.

--- board.rb
+++ board.rb
@@ -1,6 +1,8 @@
 require_relative 'tile.rb'
 
 class Board
+  NUMBER_OF_BOMBS = 9
+
   attr_accessor :grid
 
   def initialize
@@ -21,6 +23,18 @@ class Board
     grid.each_with_index do |row, row_idx|
       row.each_with_index do |col, col_idx|
         self[[row_idx,col_idx]] = Tile.new
+      end
+    end
+  end
+
+  def place_bombs
+    bombs_placed = 0
+    until bombs_placed == NUMBER_OF_BOMBS do
+      row = rand(0...9)
+      col = rand (0...9)
+      if self[[row,col]].bombed == false
+         self[[row,col]].bombed = true
+         bombs_placed += 1
       end
     end
   end
