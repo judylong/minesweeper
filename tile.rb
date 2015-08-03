da78461bb8c15911a808b47fa653b5e4021dcea4
Judy & RJ <judy+rj@gmail.com>
2015-08-03 16:11:48 -0400

    Refactor Tile#reveal logic.

--- tile.rb
+++ tile.rb
@@ -24,7 +24,17 @@ class Tile
   end
 
   def reveal
-    self.revealed = true
+    # self.revealed = true
+    queue = [self]
+    p self
+    until queue.empty?
+      #p queue
+      current = queue.shift
+      if current.neighbor_bomb_count == 0
+        queue + current.neighbors
+      end
+      current.revealed = true
+    end
   end
 
   def toggle_flag
